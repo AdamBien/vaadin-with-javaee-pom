a24cea8aa0c29f2ff763bd902641eb1cefe820a1
Adam Bien <abien@adam-bien.com>
2013-04-29 08:39:52 +0200

    Version dependency updated to 7.04 and CDI to 1

--- src/main/java/com/airhacks/presentation/WelcomePage.java
+++ src/main/java/com/airhacks/presentation/WelcomePage.java
@@ -1,8 +1,7 @@
 package com.airhacks.presentation;
 
 import com.airhacks.business.welcome.boundary.ReceptionService;
-import com.vaadin.cdi.Root;
-import com.vaadin.cdi.VaadinUI;
+import com.vaadin.cdi.CDIUI;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
@@ -13,8 +12,7 @@ import javax.inject.Inject;
  *
  * @author adam-bien.com
  */
-@Root
-@VaadinUI
+@CDIUI
 public class WelcomePage extends UI {
 
     @Inject
